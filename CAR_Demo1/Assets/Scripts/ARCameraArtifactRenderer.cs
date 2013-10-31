6b7968faa2c7dd44c878b498140dcd691843871b
Fabio Zuend <fzuend@inf.ethz.ch>
2013-10-31 11:06:59 +0100

    some cleaning up

--- CAR_Demo1/Assets/Scripts/ARCameraArtifactRenderer.cs
+++ CAR_Demo1/Assets/Scripts/ARCameraArtifactRenderer.cs
@@ -16,11 +16,10 @@ public class ARCameraArtifactRenderer : MonoBehaviour
     #region PRIVATE_MEMBER_VARIABLES
    
     // the target render texture for the camera
-    //public RenderTexture renderTex;
+    public RenderTexture renderTex;
 
     // the render material to overlay the background camera
-   // public Material renderMat;
- //   private Texture2D renderMatTex;
+    public Material renderMat;    
 
     #endregion PRIVATE_MEMBER_VARIABLES
 
@@ -33,27 +32,22 @@ public class ARCameraArtifactRenderer : MonoBehaviour
     // Use this for initialization
 	void Start () {
 
-        //Screen.SetResolution(300, 480 , true);
+        ////Screen.SetResolution(300, 480 , true);
 
-        // Setup textures and materials
+        //// Setup textures and materials
         //renderTex = new RenderTexture(1024, 1024, 16);
         //renderMat = new Material(Shader.Find("Transparent/Diffuse"));
-        //this.renderMat.mainTexture = new Texture2D(512, 512, TextureFormat.ARGB32, false);
+       
+        //// Setup camera
+        //this.camera.clearFlags = CameraClearFlags.SolidColor;
+        //this.camera.backgroundColor = new Color(0, 0, 0, 0);
+        //this.camera.targetTexture = this.renderTex;
+        //this.renderMat.mainTexture = this.renderTex;
 
-        //this.renderMatTex = new Texture2D(512, 512, TextureFormat.ARGB32, false);
-     //   this.renderMat.mainTexture = this.renderMatTex;
-
-
-        // Setup camera
-      //  this.camera.clearFlags = CameraClearFlags.SolidColor;
-       // this.camera.backgroundColor = new Color(0, 0, 0, 0);
-       // this.camera.targetTexture = this.renderTex;
-
-        // Connect render texture to other camera
-      //  ScreenOverlay bgCamScreenOverlay = this.backgroundCamera.GetComponent<ScreenOverlay>();
-      //  bgCamScreenOverlay.blendMode = ScreenOverlay.OverlayBlendMode.AlphaBlend;
-      //  bgCamScreenOverlay.textureMat = this.renderMat;
-        
+        //// Connect render texture to other camera
+        //ScreenOverlay bgCamScreenOverlay = this.backgroundCamera.GetComponent<ScreenOverlay>();
+        //bgCamScreenOverlay.blendMode = ScreenOverlay.OverlayBlendMode.AlphaBlend;
+        //bgCamScreenOverlay.textureMat = this.renderMat;  
        
         
         
@@ -66,7 +60,6 @@ public class ARCameraArtifactRenderer : MonoBehaviour
       //  renderMatTex.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
       //  renderMatTex.Apply();
         
-
         //debug
         //byte[] bytes = tex.EncodeToPNG();
         //File.WriteAllBytes("E:\\test.png", bytes);
